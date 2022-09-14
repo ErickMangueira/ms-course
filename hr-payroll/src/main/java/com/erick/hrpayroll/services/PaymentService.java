4589d79253720dc8a6af0418f8c2d6812607b5f9
Erick Mangueira <erickmangueira@gmail.com>
2022-09-13 21:29:48 -0300

    hr-payroll implamentation mock

---
+++ hr-payroll/src/main/java/com/erick/hrpayroll/services/PaymentService.java
@@ -0,0 +1,14 @@
+package com.erick.hrpayroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.erick.hrpayroll.entities.Payment;
+
+@Service
+public class PaymentService {
+
+	
+	public Payment getPayment(long workerId, int days) {
+		return new Payment("Bob",200.0, days);
+	}
+}
