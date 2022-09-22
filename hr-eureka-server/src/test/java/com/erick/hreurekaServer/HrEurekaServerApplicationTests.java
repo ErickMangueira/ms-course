0801185fbb5ab36f484c1df561821957fd8b3fb1
Erick Mangueira <erickmangueira@gmail.com>
2022-09-22 18:02:30 -0300

    project hr-eurekaServer created

---
+++ hr-eureka-server/src/test/java/com/erick/hreurekaServer/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erick.hreurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
